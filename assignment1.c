bb1008c1dd4358c731ea5a5435af9e91e0489aa1
Zaheelee <zaheelee@github.com>
2016-02-02 05:23:03 +0000

    cuserid works! yay!

--- assignment1.c
+++ assignment1.c
@@ -1,12 +1,33 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
 int main(int argc, char *argv[]){
 
-        puts(getpid());
-        puts(getppid());
-        puts(gethostname());
-        puts(cuserid());
-        puts(ctime());
-        puts(getcwd());
-        puts("this is the parent process");
+	pid_t pid = getpid();
+	pid_t ppid = getppid();
+
+	char* hostName = calloc(sizeof(char),100);
+	gethostname(hostName, 100);
+
+	char* userID = calloc(sizeof(char),100);
+	cuserid(userID);
+
+	printf("P0: PID: %d \n", (int)pid);
+	printf("P0: PPID: %d \n", (int)ppid);
+
+	printf("P0: HostName: %s \n", hostName);
+
+	printf("P0: UserID: %s \n", userID);
+
+       // puts(getpid());
+       // puts(getppid());
+       // puts(gethostname());
+       // puts(cuserid());
+       // puts(ctime());
+       // puts(getcwd());
+        puts("P0: this is the parent process");
 
         return 0;
 }
