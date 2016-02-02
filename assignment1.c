f0431e35e9f290d07d4a323cf1c9243acdc14fee
Zaheelee <zaheelee@github.com>
2016-02-02 23:52:18 +0000

    i am turning this in

--- assignment1.c
+++ assignment1.c
@@ -42,6 +42,68 @@ int main(int argc, char *argv[]){
 
 	printf("P0: WHALE is equal to  %s \n", whaleString);
 
+	int shrimp = 5;
+
+	pid_t child2pid;
+	pid_t child1pid = fork();
+
+	pid_t cpid = getpid();
+	pid_t cppid = getppid();
+	if((int)cpid == 0)
+	{
+		printf("C1: PID: %d \n", (int)cpid);
+		printf("C1: PPID: %d \n", (int)cppid);
+		shrimp = 7;
+		sleep(1);
+	}
+
+	if(getpid() == pid)
+	{
+		child2pid = fork();
+	}
+
+	if(getpid() == 0)
+	{
+		printf("C2: PID: %d \n",(int)getpid());
+		printf("C2: PPID: %d \n",(int)getppid());
+		shrimp = 6;
+		sleep(1);
+	}
+
+	int i;
+	for(i = atoi(whaleString); i > 0; i--)
+	{
+		whaleString = getenv("WHALE");
+		if(child1pid == 0)
+		{
+			printf("C1:SHRIMP = %d, WHALE is equal to  %s \n",shrimp, whaleString);
+			sprintf(whaleString, "%d", atoi(getenv("WHALE"))-1);
+			setenv("WHALE",whaleString,1);
+			shrimp = shrimp - 3;
+			i--;
+			sleep(1);
+		}
+		if(child2pid == 0)
+		{
+			printf("C2:SHRIMP = %d, WHALE is equal to  %s \n", shrimp, whaleString);
+			sprintf(whaleString, "%d", atoi(getenv("WHALE"))-1);
+			setenv("WHALE",whaleString,1);
+			shrimp = shrimp - 3;
+			i--;
+			sleep(2);
+		}
+		else
+		{
+			printf("P0:SHRIMP = %d, WHALE is equal to  %s \n", shrimp, whaleString);
+			sprintf(whaleString, "%d", atoi(getenv("WHALE"))-1);
+			setenv("WHALE", whaleString,1);
+			shrimp = shrimp - 3;
+			i--;
+		}
+		printf("end of for loop %d \n",i);
+	}
+	printf("end of for loop for realsies \n");
+
 	if(hostName) free(hostName);
 	if(userID) free(userID);
 	//if(time) free(time);
