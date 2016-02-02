0473ebc5aa5825f8a802be4c9f678ced496fb2d0
Zaheelee <zaheelee@github.com>
2016-02-02 05:55:49 +0000

    time is null, will fix in the real morning, not this 1 am hellscape. Haley is proud of me

--- assignment1.c
+++ assignment1.c
@@ -2,6 +2,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(int argc, char *argv[]){
 
@@ -14,12 +15,23 @@ int main(int argc, char *argv[]){
 	char* userID = calloc(sizeof(char),100);
 	cuserid(userID);
 
+	char* time = calloc(sizeof(char),100);
+	time_t* tempTime;
+	time = ctime(tempTime);
+
+	char* directory = calloc(sizeof(char),100);
+	getcwd(directory, 100);
+
 	printf("P0: PID: %d \n", (int)pid);
 	printf("P0: PPID: %d \n", (int)ppid);
 
 	printf("P0: HostName: %s \n", hostName);
 
 	printf("P0: UserID: %s \n", userID);
+
+	printf("P0: Time: %s \n", time);
+
+	printf("P0: Current Directory: %s \n", directory);
 
        // puts(getpid());
        // puts(getppid());
@@ -28,6 +40,11 @@ int main(int argc, char *argv[]){
        // puts(ctime());
        // puts(getcwd());
         puts("P0: this is the parent process");
+
+	if(hostName) free(hostName);
+	if(userID) free(userID);
+	if(time) free(time);
+	if(directory) free(directory);
 
         return 0;
 }
